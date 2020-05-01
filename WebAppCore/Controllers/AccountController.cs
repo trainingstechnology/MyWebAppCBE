7c8273bc561b09bd8080352ca52414129e98e987
Narayana Murthy <learntechnology@live.com>
2020-05-01 16:00:48 +0530

    first commit

---
+++ WebAppCore/Controllers/AccountController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebAppCore.Models;
+
+namespace WebAppCore.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ViewResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginViewModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                //TODO:
+                return RedirectToAction("Message");
+            }
+            else
+            { 
+                return View();
+            }
+        }
+
+        [NonAction]
+        protected object Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
---
+++ WebAppCore/Models/LoginViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppCore.Models
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Please enter Username")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Please enter Password")]
+        public string Password { get; set; }
+    }
+}
